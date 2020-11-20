177e313a1ab51ebbb581ee14ccba2523ebb0f4b6
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-20 10:39:39 +0100

    poczatek

---
+++ L04.cpp
@@ -0,0 +1,19 @@
+﻿// L04.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+#define Zadanie4A
+#ifdef Zadanie4A
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+#endif // Zadanie4A
+
+
+  
