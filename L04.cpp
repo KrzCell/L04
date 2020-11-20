664037b8961230ad4ec185751fcf512e26b8f0ff
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-20 11:18:32 +0100

    cz1

--- L04.cpp
+++ L04.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 #define Zadanie4A
@@ -10,7 +10,28 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char t[100],y[100],tekst[100];
+    cout << "Wpisz tekst -> ";
+    cin >> tekst;
+    for (int z = 0; z < strlen(tekst); z++)
+    {
+        t[z] = tekst[z];
+        for (int w = (strlen(tekst) - 1); w >= 0; w--)
+        {
+            if ((int)t[z] >= 65 && (int)t[z] <= 90)
+                y[w] = (char)((int)t[z] + 32);
+            else if ((int)t[z] >= 97 && (int)t[z] <= 122)
+                y[w] = (char)((int)t[z] - 32);
+            else
+                y[w] = t[z];
+        }
+    }
+    y[strlen(tekst)] = '\0';
+    
+    for (int z = 0; z <= strlen(t); z++)
+    {
+        cout << y[z];
+    }
 }
 
 #endif // Zadanie4A
