356046e3e31755b11c7a02d49c0670aa4afee393
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-20 12:25:28 +0100

    poprawa

--- L04.cpp
+++ L04.cpp
@@ -12,26 +12,24 @@ int main()
 {
     char t[100],y[100],tekst[100];
     cout << "Wpisz tekst -> ";
-    cin >> tekst;
+    fgets(tekst, 100, stdin);
     for (int z = 0; z < strlen(tekst); z++)
     {
         t[z] = tekst[z];
-        for (int w = (strlen(tekst) - 1); w >= 0; w--)
+        for (int w = (strlen(tekst)-1); w >= 0; w--)
         {
             if ((int)t[z] >= 65 && (int)t[z] <= 90)
-                y[w] = (char)((int)t[z] + 32);
+                y[w] = (int)(t[z] + 32);
             else if ((int)t[z] >= 97 && (int)t[z] <= 122)
-                y[w] = (char)((int)t[z] - 32);
+                y[w] = (int)(t[z] - 32);
             else
                 y[w] = t[z];
         }
     }
     y[strlen(tekst)] = '\0';
+    cout << endl;
+    cout << y;
     
-    for (int z = 0; z <= strlen(t); z++)
-    {
-        cout << y[z];
-    }
 }
 
 #endif // Zadanie4A
