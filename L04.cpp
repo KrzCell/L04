af54175ac6a2396a795073e5cafd33d93ce913c4
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-11-20 12:43:07 +0100

    p1

--- L04.cpp
+++ L04.cpp
@@ -13,10 +13,14 @@ int main()
     char t[100],y[100],tekst[100];
     cout << "Wpisz tekst -> ";
     fgets(tekst, 100, stdin);
-    for (int z = 0; z < strlen(tekst); z++)
+    int dlugosc = strlen(tekst) - 1;
+    if (tekst[dlugosc] == '\n')
+        tekst[dlugosc] = 0;
+    
+    for (int z = 0; z < dlugosc; z++)
     {
         t[z] = tekst[z];
-        for (int w = (strlen(tekst)-1); w >= 0; w--)
+        for (int w = dlugosc; w >= 0; w--)
         {
             if ((int)t[z] >= 65 && (int)t[z] <= 90)
                 y[w] = (int)(t[z] + 32);
@@ -26,9 +30,9 @@ int main()
                 y[w] = t[z];
         }
     }
-    y[strlen(tekst)] = '\0';
+    y[dlugosc] = '\0';
     cout << endl;
-    cout << y;
+   cout << y;
     
 }
 
